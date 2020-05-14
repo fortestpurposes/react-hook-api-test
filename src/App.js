fc54a0669fca76b52cbd25c157aa85b82408bd13
Dzar Bela Hanifa <dzar.bela@Dzars-MacBook-Pro.local>
2020-05-15 00:51:37 +0700

    add tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = useState({ hits: []});
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=hooks',
+      )
+      console.log(result.data)
+      setData(result.data);
+    }
+
+    fetchData()
+  }, []);
+
+  return(
+    <ul className="list">
+       {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+    </ul>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Adapter from 'enzyme-adapter-react-16';
+import MockAdapter from 'axios-mock-adapter';
+import {mount, configure} from 'enzyme';
+import axios from 'axios';
+import { act } from "react-dom/test-utils";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+test('renders list element', () => {
+  const app = mount(<App />);
+  console.log(app.debug())
+  expect(app.find('.list')).toHaveLength(1);
+});
+
+const response ={
+  data: {
+    hits: [
+      {
+        objectID: 0,
+        url: 'test0url',
+        title: 'test0title'
+      },
+      {
+        objectID: 1,
+        url: 'test1url',
+        title: 'test1title'
+      },
+      {
+        objectID: 2,
+        url: 'test2url',
+        title: 'test2title'
+      }
+    ]
+  }
+}
+
+test('check if API requests successfully done', async () => {
+  const mockAPICall = new MockAdapter(axios);
+  mockAPICall.onGet('https://hn.algolia.com/api/v1/search?query=hooks').reply(200, response.data)
+
+  let component = null;
+  await act(async () => {
+    component = mount(<App />);
+  })
+  setImmediate( () => {
+    component.update();
+    expect(component.find('a').first().prop('href')).toEqual('test0url');
+    expect(component.find('a').at(1).prop('href')).toEqual('test1url');
+    expect(component.find('a').at(2).prop('href')).toEqual('test2url');
+  })
 });
